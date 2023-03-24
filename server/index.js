b668e99f423866f05b146bec5e9a6af2ba680ff3
Franco Nicolas <franconicolas2009@live.com.ar>
2023-03-24 11:54:08 -0300

    deploy in progress

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const port = process.env.PORT || 5000;
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
